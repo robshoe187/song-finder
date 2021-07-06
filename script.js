95fe2f0639fe0264e7430321a6da66e81b3cd208
Rob Shoemaker <rob.shoe187@gmail.com>
2021-07-06 18:20:39 -0400

    added links to HTML

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var nameEl = document.querySelector("#song-name")
 var searchBtnEl = document.querySelector("#searchBtn")
-var search = nameEl.value
+var songlistEl = document.querySelector("#songlist")
+var lyricListEl = document.querySelector("#lyriclist")
 
 var searchHandler = function() {
     
@@ -19,7 +20,7 @@ fetch('https://api.spotify.com/v1/search?query='+searchTerm+'&type=track', {
             method: 'GET', headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + "BQBwCOk42eVvBpHWj2kaUR-Iwwrb3I7MgJa7NsJ63LWYw5cMRoThah07wDHD_vTen2MZqqcr3HO1D20BCZofYkwuw23oOmySoJtW35yA8qYKpMa4XH6Emtar_yMkA8hoGqmb4iFa6ADEp9DY"
+                'Authorization': 'Bearer ' + "BQAtZBs33K3DiY-z_JFsKf12zKjOj2u5y9FCeVNTGwy8_-B_Ph7QeXF86FfgRWShqUHQezjXZNAp8TzdQHpTVdLohmcEQyTe-78Y79On1NE6GiEBDWOtskD933ZYiNIkyJL0K4bSnfjcZA42"
             }
         })
             .then((response) => {
@@ -42,16 +43,16 @@ fetch("https://api.genius.com/search?q="+searchTerm+"&access_token=7A9b7GcTBshdt
 //Display Search Results from Spotify    
 var displaySpotify = function(spotifyData) {
     for (let i = 0; i < spotifyData.tracks.limit; i++) {
-        console.log(spotifyData.tracks.items[i].name + " by " + spotifyData.tracks.items[i].artists[0].name)
+        songlistEl.innerHTML += `<a href="${spotifyData.tracks.items[i].external_urls.spotify}">${spotifyData.tracks.items[i].name + " by " + spotifyData.tracks.items[i].artists[0].name}</a><br>`
     }
 }
 
 //Display search results from Genius
 var displayGenius = function(geniusData) {
     for (let i = 0; i < geniusData.response.hits.length; i++) {
-        console.log(geniusData.response.hits[i].result.full_title)
-        
+        lyricListEl.innerHTML += `<a href="${geniusData.response.hits[i].result.url}">${geniusData.response.hits[i].result.full_title}</a><br>`
     }
+
 }
 
 searchBtnEl.addEventListener("click", searchHandler)
